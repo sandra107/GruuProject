b8067214ea5a1abcf8ce2fd729f5cc5bbfdeadff
Lương Thị Thu Hà (ITBU KT) <haltt32@viettelimex.vn>
2022-10-07 17:50:54 +0700

    a

--- src/main/java/testScript/Script3.java
+++ src/main/java/testScript/Script3.java
@@ -1,29 +1,29 @@
 package testScript;
 
+import Help.browserConfiguration.configReader.ObjectReader;
 import Help.logger.LoggerHelper;
 import org.apache.log4j.Logger;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import pages.Day2;
 import pages.Day3;
 import testBase.TestBase;
 
 public class Script3 extends TestBase {
     private final Logger log = LoggerHelper.getLogger(Script3.class);
 
-    Day3 day3;
-
     @DataProvider(name = "testData")
-    public Object[][] testData(){
-        Object[][] data = getExcelData("testData.xlxs", "loginData");
+    public Object[][] testData() throws Exception {
+        Object[][] data = getExcelData("testData.xlsx", "loginData");
         return data;
     }
 
     @Test(dataProvider = "testData")
     public void loginToApplication(String uid, String password){
-        for(int i = 0; i<data.length; i++){
-            uid = data[i][0];
-
-        }
+        getApplicationUrl(ObjectReader.reader.getUrl());
+        Day3 day2 = new Day3(driver);
+        day2.loginToApplication(uid,password);
+        System.out.println("Ok rồi má ơi");
     }
 
 }

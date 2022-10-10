02919226cb79af92992a42455f467ba4c42bc2c4
Lương Thị Thu Hà (ITBU KT) <haltt32@viettelimex.vn>
2022-10-10 17:59:42 +0700

    â

--- src/main/java/testScript/Script1.java
+++ src/main/java/testScript/Script1.java
@@ -3,14 +3,10 @@ package testScript;
 import Help.assertion.AssertionHelper;
 import Help.browserConfiguration.configReader.ObjectReader;
 import Help.logger.LoggerHelper;
-import com.sun.org.glassfish.gmbal.Description;
 import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 import pages.Day1;
 import testBase.TestBase;
-
-import javax.management.Descriptor;
-import javax.swing.*;
 
 public class Script1 extends TestBase {
     private final Logger log = LoggerHelper.getLogger(Script1.class);
---
+++ src/main/java/utils/Util.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Util {
+    public static final String EXPECT_TITLE = "Guru99 Bank Manager HomePage";
+    public static final String EXPECT_ERROR_LOGIN = "User or Password is not valid";
+}
